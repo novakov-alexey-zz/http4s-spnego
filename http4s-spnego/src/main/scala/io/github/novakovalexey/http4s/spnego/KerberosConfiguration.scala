9aa299caabab89ceef306a1d8dd5032e4094feca
Alexey Novakov <novakov.alex@gmail.com>
2019-09-20 11:34:18 +0200

    use java hashmap directly

--- http4s-spnego/src/main/scala/io/github/novakovalexey/http4s/spnego/KerberosConfiguration.scala
+++ http4s-spnego/src/main/scala/io/github/novakovalexey/http4s/spnego/KerberosConfiguration.scala
@@ -1,29 +1,30 @@
 package io.github.novakovalexey.http4s.spnego
 
-import javax.security.auth.login.{AppConfigurationEntry, Configuration}
-// scala 2.12 needs to be supported
-import collection.JavaConverters._
+import java.util
 
-case class KerberosConfiguration(keytab: String, principal: String, debug: Boolean, ticketCache: Option[String]) extends Configuration {
+import javax.security.auth.login.{AppConfigurationEntry, Configuration}
+
+case class KerberosConfiguration(keytab: String, principal: String, debug: Boolean, ticketCache: Option[String])
+    extends Configuration {
+
+  val cfg = new util.HashMap[String, String]()
+  cfg.put("keyTab", keytab)
+  cfg.put("principal", principal)
+  cfg.put("useKeyTab", "true")
+  cfg.put("storeKey", "true")
+  cfg.put("doNotPrompt", "true")
+  cfg.put("useTicketCache", "true")
+  cfg.put("renewTGT", "true")
+  cfg.put("isInitiator", "false")
+  cfg.put("refreshKrb5Config", "true")
+  cfg.put("debug", debug.toString)
+  ticketCache.foreach(tc => cfg.put("ticketCache", tc))
 
   override def getAppConfigurationEntry(name: String): Array[AppConfigurationEntry] = Array(
     new AppConfigurationEntry(
       "com.sun.security.auth.module.Krb5LoginModule",
       AppConfigurationEntry.LoginModuleControlFlag.REQUIRED,
-      (Map(
-        "keyTab" -> keytab,
-        "principal" -> principal,
-        "useKeyTab" -> "true",
-        "storeKey" -> "true",
-        "doNotPrompt" -> "true",
-        "useTicketCache" -> "true",
-        "renewTGT" -> "true",
-        "isInitiator" -> "false",
-        "refreshKrb5Config" -> "true",
-        "debug" -> debug.toString
-      ) ++ ticketCache.map { x =>
-        Map("ticketCache" -> x)
-      }.getOrElse(Map.empty)).asJava
+      cfg
     )
   )
 }
