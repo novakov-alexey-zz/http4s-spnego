a4c065c70061e2e0ceede663b61168884c4a89ec
Alexey Novakov <novakov.alex@gmail.com>
2019-09-20 10:24:43 +0200

    check when cookie is expired

--- http4s-spnego/src/test/scala/io/github/novakovalexey/http4s/spnego/SpnegoAuthTest.scala
+++ http4s-spnego/src/test/scala/io/github/novakovalexey/http4s/spnego/SpnegoAuthTest.scala
@@ -25,8 +25,8 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   val cookieName = "http4s.spnego"
 
   val cfg = SpnegoConfig(principal, realm, keytab, debug, None, "secret", domain, path, tokenValidity, cookieName)
-  val authentication = new Spnego[IO](cfg)
-  val login = new LoginEndpoint[IO](authentication)
+  val spnego = new Spnego[IO](cfg)
+  val login = new LoginEndpoint[IO](spnego)
 
   val userPrincipal = "myprincipal"
   val testTokens = new Tokens(cfg.tokenValidity.toMillis, Codec.toUTF8(cfg.signatureSecret))
@@ -34,8 +34,8 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   it should "reject invalid authorization token" in {
     val req = Request[IO]().putHeaders(Authorization(Credentials.Token(SpnegoAuthenticator.Negotiate.ci, "test")))
 
-    val routes = login.routes.orNotFound
-    val res = routes.run(req)
+    val route = login.routes.orNotFound
+    val res = route.run(req)
     val actualResp = res.unsafeRunSync
 
     actualResp.status should ===(Status.Unauthorized)
@@ -45,15 +45,15 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   it should "reject invalid cookie" in {
     val req = Request[IO]().addCookie(cookieName, "myprincipal&1566120533815&0zjbRRVXDFlDYfRurlxaySKWhgE=")
 
-    val routes = login.routes.orNotFound
-    val res = routes.run(req)
+    val route = login.routes.orNotFound
+    val res = route.run(req)
     val actualResp = res.unsafeRunSync
 
     actualResp.status should ===(Status.Unauthorized)
     actualResp.as[String].unsafeRunSync() should include("Failed to parse ")
   }
 
-  def mockKerberos(token: Option[Token]): Spnego[IO] = {
+  def mockKerberos(token: Option[Token], tokens: Tokens = testTokens): Spnego[IO] = {
     new Spnego[IO](cfg) {
       override val tokens: Tokens = testTokens
       override val authenticator: SpnegoAuthenticator = new SpnegoAuthenticator(cfg, tokens) {
@@ -65,15 +65,12 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   }
 
   it should "reject token if Kerberos failed" in {
-    val authentication = mockKerberos(None)
-    val login = new LoginEndpoint[IO](authentication)
-    val routes = login.routes.orNotFound
-
     //given
+    val route =  loginRoute(None)
     val clientToken = "test"
     val req2 = Request[IO]().putHeaders(Header("Authorization", s"${SpnegoAuthenticator.Negotiate} $clientToken"))
     //when
-    val res2 = routes.run(req2)
+    val res2 = route.run(req2)
     val actualResp = res2.unsafeRunSync
     //then
     actualResp.status should ===(Status.Unauthorized)
@@ -81,13 +78,12 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   }
 
   it should "reject invalid number of fields in cookie token" in {
-    val login = new LoginEndpoint[IO](authentication)
-    val routes = login.routes.orNotFound
+    val route = loginRoute(None)
 
     //given
     val req = Request[IO]().addCookie(RequestCookie(cookieName, s"$userPrincipal&"))
     //when
-    val res = routes.run(req)
+    val res = route.run(req)
     //then
     val actualResp = res.unsafeRunSync()
     actualResp.status should ===(Status.Unauthorized)
@@ -95,13 +91,12 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   }
 
   it should "reject invalid expiration parameter in cookie token" in {
-    val login = new LoginEndpoint[IO](authentication)
-    val routes = login.routes.orNotFound
+    val route = loginRoute(None)
 
     //given
     val req = Request[IO]().addCookie(RequestCookie(cookieName, s"$userPrincipal&a&b"))
     //when
-    val res = routes.run(req)
+    val res = route.run(req)
     //then
     val actualResp = res.unsafeRunSync()
     actualResp.status should ===(Status.Unauthorized)
@@ -109,15 +104,11 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
   }
 
   it should "return authenticated token" in {
-    val authentication = mockKerberos(Some(testTokens.create(userPrincipal)))
-
-    val login = new LoginEndpoint[IO](authentication)
-    val routes = login.routes.orNotFound
-
     //given
+    val route = loginRoute(Some(testTokens.create(userPrincipal)))
     val initReq = Request[IO]()
     //when
-    val res1 = routes.run(initReq)
+    val res1 = route.run(initReq)
     //then
     res1.unsafeRunSync().status should ===(Status.Unauthorized)
 
@@ -125,8 +116,8 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
     val clientToken = "test"
     val req2 = Request[IO]().putHeaders(Header("Authorization", s"${SpnegoAuthenticator.Negotiate} $clientToken"))
     //when
-    val res2 = routes.run(req2)
-    val okResponse = res2.unsafeRunSync
+    val okResponse = route.run(req2).unsafeRunSync
+
     //then
     okResponse.status should ===(Status.Ok)
     okResponse.cookies.length should ===(1)
@@ -141,13 +132,42 @@ class SpnegoAuthTest extends FlatSpec with Matchers {
     //given
     val req3 = Request[IO]().addCookie(RequestCookie(cookieName, cookie))
     //when
-    val res3 = routes.run(req3)
+    val res3 = route.run(req3).unsafeRunSync()
     //then
-    res3.unsafeRunSync().status should ===(Status.Ok)
+    res3.status should ===(Status.Ok)
   }
 
-  it should "use onFailure function" in {
+  it should "reject expired token inside the cookie" in {
+    //given
+    val noTokenValidity = new Tokens(0.millisecond.toMillis, Codec.toUTF8(cfg.signatureSecret))
+    val routes = loginRoute(Some(noTokenValidity.create(userPrincipal)), noTokenValidity)
+    val signature = "test"
+    val req = Request[IO]().putHeaders(Header("Authorization", s"${SpnegoAuthenticator.Negotiate} $signature"))
+    //when
+    val io = routes.run(req)
+    val resp = io.unsafeRunSync
+    //then
+    resp.status should ===(Status.Ok)
+    val cookie = resp.cookies.headOption.map(_.content).getOrElse(fail())
+    val token = noTokenValidity.parse(cookie)
+    token.isRight should ===(true)
+
+    //given
+    val req3 = Request[IO]().addCookie(RequestCookie(cookieName, cookie))
+    //when
+    val res3 = routes.run(req3)
+    //then
+    res3.unsafeRunSync().status should ===(Status.Unauthorized)
+  }
+
+  def loginRoute(token: Option[Token], tokens: Tokens = testTokens): Kleisli[IO, Request[IO], Response[IO]] = {
+    val authentication = mockKerberos(token)
     val login = new LoginEndpoint[IO](authentication)
+    login.routes.orNotFound
+  }
+
+  it should "allow custom onFailure hander" in {
+    val login = new LoginEndpoint[IO](spnego)
     val onFailure: AuthedRoutes[Rejection, IO] = Kleisli { _ =>
       val res = Response[IO](Status.BadRequest).putHeaders(Header("test 1", "test 2")).withEntity("test entity")
       OptionT.liftF(res.pure[IO])
